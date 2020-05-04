c55655bdd477f7e87abec9d04f7704943169e4e6
Gabriel Marko <marko.gabriel.cz@gmail.com>
2020-05-04 17:35:56 +0200

    mixbonacci - use int64 instead of int

--- mixbonacci/mixbonacci.go
+++ mixbonacci/mixbonacci.go
@@ -1,34 +1,33 @@
 package mixbonacci
 
 type generator struct {
-	state       []int
-	computeNext func([]int) int
+	state       []int64
+	computeNext func([]int64) int64
 }
 
-func (g *generator) next() int {
+func (g *generator) next() int64 {
 	r := g.state[0]
 	n := g.computeNext(g.state)
 	g.state = append(g.state[1:], n)
 	return r
 }
 
-func Mixbonacci(pattern []string, length int) []int {
+func Mixbonacci(pattern []string, length int) []int64 {
 	if length == 0 || len(pattern) == 0 {
-		return []int{}
+		return []int64{}
 	}
 	generators := map[string]*generator{
-		"fib": &generator{[]int{0, 1}, func(a []int) int { return a[0] + a[1] }},
-		"pad": &generator{[]int{1, 0, 0}, func(a []int) int { return a[0] + a[1] }},
-		"jac": &generator{[]int{0, 1}, func(a []int) int { return 2*a[0] + a[1] }},
-		"pel": &generator{[]int{0, 1}, func(a []int) int { return a[0] + 2*a[1] }},
-		"tri": &generator{[]int{0, 0, 1}, func(a []int) int { return a[0] + a[1] + a[2] }},
-		"tet": &generator{[]int{0, 0, 0, 1}, func(a []int) int { return a[0] + a[1] + a[2] + a[3] }},
+		"fib": &generator{[]int64{0, 1}, func(a []int64) int64 { return a[0] + a[1] }},
+		"pad": &generator{[]int64{1, 0, 0}, func(a []int64) int64 { return a[0] + a[1] }},
+		"jac": &generator{[]int64{0, 1}, func(a []int64) int64 { return 2*a[0] + a[1] }},
+		"pel": &generator{[]int64{0, 1}, func(a []int64) int64 { return a[0] + 2*a[1] }},
+		"tri": &generator{[]int64{0, 0, 1}, func(a []int64) int64 { return a[0] + a[1] + a[2] }},
+		"tet": &generator{[]int64{0, 0, 0, 1}, func(a []int64) int64 { return a[0] + a[1] + a[2] + a[3] }},
 	}
-	result := make([]int, 0, length)
+	result := make([]int64, 0, length)
 	pLength := len(pattern)
 	for i := 0; i < length; i++ {
 		result = append(result, generators[pattern[i%pLength]].next())
 	}
 	return result
-	return []int{}
 }
--- mixbonacci/mixbonacci_test.go
+++ mixbonacci/mixbonacci_test.go
@@ -9,19 +9,19 @@ import (
 
 var _ = Describe("Test Suite", func() {
 	It("Edge Cases", func() {
-		Expect(Mixbonacci([]string{}, 10)).Should(Equal([]int{}))
-		Expect(Mixbonacci([]string{"fib"}, 0)).Should(Equal([]int{}))
+		Expect(Mixbonacci([]string{}, 10)).Should(Equal([]int64{}))
+		Expect(Mixbonacci([]string{"fib"}, 0)).Should(Equal([]int64{}))
 	})
 	It("Single-element Patterns", func() {
-		Expect(Mixbonacci([]string{"fib"}, 10)).Should(Equal([]int{0, 1, 1, 2, 3, 5, 8, 13, 21, 34}))
-		Expect(Mixbonacci([]string{"pad"}, 10)).Should(Equal([]int{1, 0, 0, 1, 0, 1, 1, 1, 2, 2}))
-		Expect(Mixbonacci([]string{"jac"}, 10)).Should(Equal([]int{0, 1, 1, 3, 5, 11, 21, 43, 85, 171}))
-		Expect(Mixbonacci([]string{"pel"}, 10)).Should(Equal([]int{0, 1, 2, 5, 12, 29, 70, 169, 408, 985}))
-		Expect(Mixbonacci([]string{"tri"}, 10)).Should(Equal([]int{0, 0, 1, 1, 2, 4, 7, 13, 24, 44}))
-		Expect(Mixbonacci([]string{"tet"}, 10)).Should(Equal([]int{0, 0, 0, 1, 1, 2, 4, 8, 15, 29}))
+		Expect(Mixbonacci([]string{"fib"}, 10)).Should(Equal([]int64{0, 1, 1, 2, 3, 5, 8, 13, 21, 34}))
+		Expect(Mixbonacci([]string{"pad"}, 10)).Should(Equal([]int64{1, 0, 0, 1, 0, 1, 1, 1, 2, 2}))
+		Expect(Mixbonacci([]string{"jac"}, 10)).Should(Equal([]int64{0, 1, 1, 3, 5, 11, 21, 43, 85, 171}))
+		Expect(Mixbonacci([]string{"pel"}, 10)).Should(Equal([]int64{0, 1, 2, 5, 12, 29, 70, 169, 408, 985}))
+		Expect(Mixbonacci([]string{"tri"}, 10)).Should(Equal([]int64{0, 0, 1, 1, 2, 4, 7, 13, 24, 44}))
+		Expect(Mixbonacci([]string{"tet"}, 10)).Should(Equal([]int64{0, 0, 0, 1, 1, 2, 4, 8, 15, 29}))
 	})
 	It("Multi-element Patterns", func() {
-		Expect(Mixbonacci([]string{"fib", "tet"}, 10)).Should(Equal([]int{0, 0, 1, 0, 1, 0, 2, 1, 3, 1}))
-		Expect(Mixbonacci([]string{"jac", "jac", "pel"}, 10)).Should(Equal([]int{0, 1, 0, 1, 3, 1, 5, 11, 2, 21}))
+		Expect(Mixbonacci([]string{"fib", "tet"}, 10)).Should(Equal([]int64{0, 0, 1, 0, 1, 0, 2, 1, 3, 1}))
+		Expect(Mixbonacci([]string{"jac", "jac", "pel"}, 10)).Should(Equal([]int64{0, 1, 0, 1, 3, 1, 5, 11, 2, 21}))
 	})
 })
