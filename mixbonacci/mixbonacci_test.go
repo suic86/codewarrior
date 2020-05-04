53307f2b12bc39cc72ad0e19c2bb011e6c843dde
Gabriel Marko <marko.gabriel.cz@gmail.com>
2020-05-04 17:55:15 +0200

    mixbonacci - add random tests

--- mixbonacci/mixbonacci_test.go
+++ mixbonacci/mixbonacci_test.go
@@ -1,6 +1,8 @@
 package mixbonacci_test
 
 import (
+	"math/rand"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
@@ -24,4 +26,53 @@ var _ = Describe("Test Suite", func() {
 		Expect(Mixbonacci([]string{"fib", "tet"}, 10)).Should(Equal([]int64{0, 0, 1, 0, 1, 0, 2, 1, 3, 1}))
 		Expect(Mixbonacci([]string{"jac", "jac", "pel"}, 10)).Should(Equal([]int64{0, 1, 0, 1, 3, 1, 5, 11, 2, 21}))
 	})
+	It("Random Tests", func() {
+		for i := 0; i < 100; i++ {
+			pattern, length := randomInput()
+			expected := reference(pattern, length)
+			Expect(Mixbonacci(pattern, length)).Should(Equal(expected))
+		}
+	})
 })
+
+type generator struct {
+	state       []int64
+	computeNext func([]int64) int64
+}
+
+func (g *generator) next() int64 {
+	r := g.state[0]
+	n := g.computeNext(g.state)
+	g.state = append(g.state[1:], n)
+	return r
+}
+
+func reference(pattern []string, length int) []int64 {
+	if length == 0 || len(pattern) == 0 {
+		return []int64{}
+	}
+	generators := map[string]*generator{
+		"fib": &generator{[]int64{0, 1}, func(a []int64) int64 { return a[0] + a[1] }},
+		"pad": &generator{[]int64{1, 0, 0}, func(a []int64) int64 { return a[0] + a[1] }},
+		"jac": &generator{[]int64{0, 1}, func(a []int64) int64 { return 2*a[0] + a[1] }},
+		"pel": &generator{[]int64{0, 1}, func(a []int64) int64 { return a[0] + 2*a[1] }},
+		"tri": &generator{[]int64{0, 0, 1}, func(a []int64) int64 { return a[0] + a[1] + a[2] }},
+		"tet": &generator{[]int64{0, 0, 0, 1}, func(a []int64) int64 { return a[0] + a[1] + a[2] + a[3] }},
+	}
+	result := make([]int64, 0, length)
+	pLength := len(pattern)
+	for i := 0; i < length; i++ {
+		result = append(result, generators[pattern[i%pLength]].next())
+	}
+	return result
+}
+
+func randomInput() ([]string, int) {
+	sequences := []string{"fib", "pad", "jac", "pel", "tri", "tet"}
+	patternLength := rand.Intn(20)
+	var pattern []string
+	for i := 0; i < patternLength; i++ {
+		pattern = append(pattern, sequences[rand.Intn(6)])
+	}
+	return pattern, rand.Intn(51)
+}
